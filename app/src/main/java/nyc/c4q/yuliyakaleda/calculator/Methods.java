6af553ebc00c65701c3461227f17adc69ce4893b
Yuliya-Kaleda <yulia.koleda1988@gmail.com>
2015-05-08 00:22:24 -0400

    simple interface

---
+++ app/src/main/java/nyc/c4q/yuliyakaleda/calculator/Methods.java
@@ -0,0 +1,14 @@
+package nyc.c4q.yuliyakaleda.calculator;
+
+/**
+ * Created by July on 5/5/15.
+ */
+public class Methods {
+    long result = 0;
+
+  public static long plus(int a, int b) {
+      long result = a + b;
+
+      return result;
+  }
+}
